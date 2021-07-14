997719ae18b17af049a33f07c7a49ceb2f183c4e
jihuanyu <ji2240224966@163.com>
2021-07-14 14:01:58 +0800

    'insurance'

--- src/pages/insurance/index.jsx
+++ src/pages/insurance/index.jsx
@@ -4,15 +4,18 @@ import Banner from '@/components/insurance/banner';
 import ShowData from '@/components/insurance/shwodata';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+
 class insurance extends Component {
     render() {
         return (
-            <div className='insurance_container'>
+            <>
                 <Header />
-                <Banner />
-                <ShowData />
+                <div className='insurance_container'>
+                    <Banner />
+                    <ShowData />
+                </div>
                 <Footer />
-            </div>
+            </>
         );
     }
 }
