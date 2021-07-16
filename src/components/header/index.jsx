23a05b1e21b77e0851fed9aafd606c3d119a9b8a
jihuanyu <ji2240224966@163.com>
2021-07-16 15:58:39 +0800

    'insuranceH5'

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react'
-import './index.less'
-import LOGO from '../../assets/images/logo.svg'
-import GuradSvg from '../../assets/images/gurad-icon.svg'
-import MenuSvg from '../../assets/images/menu.svg'
-import {useWeb3React} from '@web3-react/core'
-import {formatAddress} from '../../utils'
-import DrawerMenu from '../drawer-menu'
-import {FormattedMessage} from 'react-intl'
-import ConnectWallDialog from '../dialogs/connect-wallet-dialog'
-import {Link} from 'react-router-dom'
-import InstallMetamaskDialog from '../dialogs/install-metamask-dialog'
+import React, { useState } from 'react';
+import './index.less';
+import LOGO from '../../assets/images/logo.svg';
+import GuradSvg from '../../assets/images/gurad-icon.svg';
+import MenuSvg from '../../assets/images/menu.svg';
+import { useWeb3React } from '@web3-react/core';
+import { formatAddress } from '../../utils';
+import DrawerMenu from '../drawer-menu';
+import { FormattedMessage } from 'react-intl';
+import ConnectWallDialog from '../dialogs/connect-wallet-dialog';
+import { Link } from 'react-router-dom';
+import InstallMetamaskDialog from '../dialogs/install-metamask-dialog';
 
 export const navList = [
     {
@@ -36,63 +36,92 @@ export const navList = [
     },
 ];
 
-function Header (props) {
-  console.log(props)
-  const {account, activate, active} = useWeb3React()
-  console.log(account, active)
-  const [visibleMenu, setVisibleMenu] = useState(false)
-  const [visibleConnectWall, setVisibleConnectWall] = useState(false)
-  const [nonExistentMetamask, setNonExistentMetamask] = useState(false)
-  const connectWalletClick = () => {
-    if (window.ethereum) {
-      setVisibleConnectWall(true)
-    } else {
-      setNonExistentMetamask(true)
-    }
-  }
-  return (
-    <>
-      <div className="header">
-        <div className="logo">
-          <img src={LOGO} alt="Gurad"/>
-        </div>
-        <ul className="nav_list">
-          {
-            navList.map(item => (
-              <Link to={item.path} className="nav_list_item" key={item.path}>
-                <span className={props.location.pathname === item.path ? 'active' : ''}>
-                  {item.name}
-                </span>
-              </Link>
-            ))
-          }
-        </ul>
-        <div className="connect_wallet">
-          {!active ? (
-            <div className="not_connect flex_center" onClick={connectWalletClick}>CONNECT WALLET</div>
-          ) : (
-            <div className="connected">
-              <div className="balance flex_center">
-                <img src={GuradSvg} alt=""/>
-                14966231.12
-              </div>
-              <div className="address flex_center">
-                {formatAddress(account)}
-                <span/>
-              </div>
+function Header(props) {
+    console.log(props);
+    const { account, activate, active } = useWeb3React();
+    console.log(account, active);
+    const [visibleMenu, setVisibleMenu] = useState(false);
+    const [visibleConnectWall, setVisibleConnectWall] = useState(false);
+    const [nonExistentMetamask, setNonExistentMetamask] = useState(false);
+    const connectWalletClick = () => {
+        if (window.ethereum) {
+            setVisibleConnectWall(true);
+        } else {
+            setNonExistentMetamask(true);
+        }
+    };
+    return (
+        <>
+            <div className='header'>
+                <div className='logo'>
+                    <img src={LOGO} alt='Gurad' />
+                </div>
+                <ul className='nav_list'>
+                    {navList.map((item) => (
+                        <Link
+                            to={item.path}
+                            className='nav_list_item'
+                            key={item.path}>
+                            <span
+                                className={
+                                    props.location.pathname === item.path
+                                        ? 'active'
+                                        : ''
+                                }>
+                                {item.name}
+                            </span>
+                        </Link>
+                    ))}
+                </ul>
+                <div className='connect_wallet'>
+                    {!active ? (
+                        <div
+                            className='not_connect flex_center'
+                            onClick={connectWalletClick}>
+                            CONNECT WALLET
+                        </div>
+                    ) : (
+                        <div className='connected'>
+                            <div className='balance flex_center'>
+                                <img src={GuradSvg} alt='' />
+                                14966231.12
+                            </div>
+                            <div className='address flex_center'>
+                                {formatAddress(account)}
+                                <span />
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <div className='menu_tab'>
+                    <img
+                        src={MenuSvg}
+                        alt=''
+                        onClick={() => setVisibleMenu(true)}
+                    />
+                </div>
             </div>
-          )}
-        </div>
-        <div className="menu_tab">
-          <img src={MenuSvg} alt="" onClick={() => setVisibleMenu(true)}/>
-        </div>
-      </div>
-      <DrawerMenu {...{account, active, visible: visibleMenu, setVisible: setVisibleMenu, connectWalletClick, ...props}}/>
-      <ConnectWallDialog visible={visibleConnectWall} onClose={() => setVisibleConnectWall(false)}/>
-      {/* 无钱包提示 */}
-      <InstallMetamaskDialog visible={nonExistentMetamask} onClose={() => setNonExistentMetamask(false)}/>
-    </>
-  )
+            <DrawerMenu
+                {...{
+                    account,
+                    active,
+                    visible: visibleMenu,
+                    setVisible: setVisibleMenu,
+                    connectWalletClick,
+                    ...props,
+                }}
+            />
+            <ConnectWallDialog
+                visible={visibleConnectWall}
+                onClose={() => setVisibleConnectWall(false)}
+            />
+            {/* 无钱包提示 */}
+            <InstallMetamaskDialog
+                visible={nonExistentMetamask}
+                onClose={() => setNonExistentMetamask(false)}
+            />
+        </>
+    );
 }
 
 export default Header;
