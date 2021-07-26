35bbd0eaacf9ae4b7e4c46f172928e90591ae4c2
ydev <ydev@gmail.com>
2021-07-26 17:33:24 +0800

    update

--- src/locales/zh_CN.js
+++ src/locales/zh_CN.js
@@ -35,6 +35,7 @@ export default {
   header_text6: '连接钱包',
   header_text7: '转到 {name}',
   header_text8: '扫码连接',
+  header_text9: '保险',
   install_metamask_dialog_text1: '您需要安装{wallet}才能继续。安装后，请点击',
   install_metamask_dialog_text2: '刷新页面',
   install_metamask_dialog_text3: '选择一个钱包',
