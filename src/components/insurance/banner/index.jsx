f54986ae4d8b428c2fca0807be638a7114a47fb1
sunxuan <1406243464@qq.com>
2021-07-27 23:59:02 +0800

    banner

--- src/components/insurance/banner/index.jsx
+++ src/components/insurance/banner/index.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react'
 import './index.less'
 import { Carousel } from 'antd'
-const Banner = props => {
-  return (
-    <Carousel autoplay>
-      <div className='insurance_banner'>
-        <div className='insurance_banner_wrap insurance_banner_wrap_2'></div>
-      </div>
-      <div className='insurance_banner'>
-        <div className='insurance_banner_wrap insurance_banner_wrap_1'></div>
-      </div>
-    </Carousel>
-  )
-}
+const Banner = (props) => (
+  <Carousel autoplay={true}>
+    <div className='insurance_banner'>
+      <div className='insurance_banner_wrap insurance_banner_wrap_2'></div>
+    </div>
+    <div className='insurance_banner'>
+      <div className='insurance_banner_wrap insurance_banner_wrap_1'></div>
+    </div>
+  </Carousel>
+)
 
 export default Banner
