ce4a11eedd3022eae08424963f778fbff8aba30e
sunxuan <1406243464@qq.com>
2021-07-27 20:21:24 +0800

    banner

--- src/components/insurance/banner/index.jsx
+++ src/components/insurance/banner/index.jsx
@@ -5,10 +5,10 @@ const Banner = props => {
   return (
     <Carousel autoplay>
       <div className='insurance_banner'>
-        <div className='insurance_banner_wrap insurance_banner_wrap_1'></div>
+        <div className='insurance_banner_wrap insurance_banner_wrap_2'></div>
       </div>
       <div className='insurance_banner'>
-        <div className='insurance_banner_wrap insurance_banner_wrap_2'></div>
+        <div className='insurance_banner_wrap insurance_banner_wrap_1'></div>
       </div>
     </Carousel>
   )
